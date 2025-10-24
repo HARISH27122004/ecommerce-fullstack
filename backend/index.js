469c4cabd4033a7579485712450702d386bed1be
harish <harishcr320@gmail.com>
2025-10-24 13:52:42 +0530

    new change

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ const router = require('./routes/route.js')
 const path = require('path')
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}));
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../frontend')))
 
